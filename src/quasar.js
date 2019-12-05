d839d1580fec36c1c35ca10a3051a53781987c2d
Adrien Croquelois <croquelois.adrien@gmail.com>
2019-12-05 22:23:31 +0100

    starting project

---
+++ src/quasar.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+
+import 'quasar/dist/quasar.css'
+import iconSet from 'quasar/icon-set/fontawesome-v5.js'
+import '@quasar/extras/roboto-font/roboto-font.css'
+import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
+import { Quasar } from 'quasar'
+
+Vue.use(Quasar, {
+  config: {},
+  components: { /* not needed if importStrategy is not 'manual' */ },
+  directives: { /* not needed if importStrategy is not 'manual' */ },
+  plugins: {
+  },
+  iconSet: iconSet
+ })
